8a643f76cefb3c8fddebf614a6fcd37ecf106e17
kudaykiran-99 <uday@dkgrouplabs.com>
2026-01-04 09:45:48 +0530

    ie poi mapping changes

--- src/main/java/com/sarthi/repository/IeFieldsMappingRepository.java
+++ src/main/java/com/sarthi/repository/IeFieldsMappingRepository.java
@@ -2,6 +2,7 @@ package com.sarthi.repository;
 
 import com.sarthi.entity.IEFieldsMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +10,21 @@ import java.util.Optional;
 @Repository
 public interface IeFieldsMappingRepository extends JpaRepository<IEFieldsMapping, Long> {
     Optional<IEFieldsMapping> findByPinCodeAndProductAndStage(String pinCode, String product, String stage);
+
+    @Query("""
+SELECT m FROM IEFieldsMapping m
+WHERE m.pinCode = :pinCode
+  AND m.product = :product
+  AND (
+       m.stage = :stage
+       OR m.stage LIKE %:stage%
+  )
+""")
+    Optional<IEFieldsMapping> findByPinCodeProductAndStageMatch(
+            String pinCode,
+            String product,
+            String stage
+    );
+
+
 }
--- src/main/java/com/sarthi/repository/IePincodePoiMappingRepository.java
+++ src/main/java/com/sarthi/repository/IePincodePoiMappingRepository.java
@@ -2,13 +2,46 @@ package com.sarthi.repository;
 
 import com.sarthi.entity.IePincodePoiMapping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface IePincodePoiMappingRepository extends JpaRepository<IePincodePoiMapping, Long> {
-    Optional<Integer> findPrimaryIe(String pinCode, String product, String poiCode, String rio);
+   // Optional<Integer> findPrimaryIe(String pinCode, String product, String poiCode, String rio);
 
-    Optional<Integer> findSecondaryIe(String pinCode, String product, String poiCode, String rio);
+   // Optional<Integer> findSecondaryIe(String pinCode, String product, String poiCode, String rio);
+
+
+    @Query("""
+    SELECT m.employeeCode
+    FROM IePincodePoiMapping m
+    WHERE m.pinCode = :pinCode
+      AND m.product = :product
+      AND m.poiCode = :poiCode
+      AND m.ieType = 'PRIMARY'
+""")
+    Optional<String> findPrimaryIe(
+            String pinCode,
+            String product,
+            String poiCode
+    );
+
+    @Query("""
+    SELECT m.employeeCode
+    FROM IePincodePoiMapping m
+    WHERE m.pinCode = :pinCode
+      AND m.product = :product
+      AND m.poiCode = :poiCode
+      AND m.ieType = 'SECONDARY'
+""")
+    Optional<String> findSecondaryIe(
+            String pinCode,
+            String product,
+            String poiCode
+    );
+
+
+
 }
