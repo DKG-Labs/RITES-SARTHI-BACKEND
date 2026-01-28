533a49d42ff3b21010d780df08c7476fb08f19e0
riyansh-dkg <riyansh@dkgrouplabs.com>
2026-01-28 16:17:56 +0530

    final ic correct fetching

--- src/main/java/com/sarthi/service/FinalInspectionCallService.java
+++ src/main/java/com/sarthi/service/FinalInspectionCallService.java
@@ -48,5 +48,58 @@ public interface FinalInspectionCallService {
      * @return List of lot numbers
      */
     List<String> getLotNumbersByRmIcNumber(String rmIcNumber);
+
+    // ==================== NEW METHODS FOR REVERSED DROPDOWN FLOW ====================
+
+    /**
+     * Get RM IC certificate numbers for Final Inspection Call dropdown
+     * Returns CERTIFICATE_NO (e.g., "N/ER-01120005/RAJK") for display
+     * @param vendorId - Vendor ID to filter
+     * @return List of RM IC certificate numbers
+     */
+    List<String> getRmIcCertificateNumbers(String vendorId);
+
+    /**
+     * Get Process IC certificate numbers by RM IC certificate
+     * Returns CERTIFICATE_NO (e.g., "N/EP-01170002/RAJK") for Process ICs that used the specified RM IC
+     * @param rmCertificateNo - RM IC certificate number (e.g., "N/ER-01120005/RAJK")
+     * @return List of Process IC certificate numbers
+     */
+    List<String> getProcessIcCertificateNumbersByRmCertificate(String rmCertificateNo);
+
+    /**
+     * Get Lot numbers by RM IC and Process IC certificate numbers
+     * Converts certificates to IC numbers internally, then finds lots
+     * @param rmCertificateNo - RM IC certificate number (e.g., "N/ER-01120005/RAJK")
+     * @param processCertificateNo - Process IC certificate number (e.g., "N/EP-01170002/RAJK")
+     * @return List of lot numbers
+     */
+    List<String> getLotNumbersByRmAndProcessCertificates(String rmCertificateNo, String processCertificateNo);
+
+    /**
+     * Get Heat numbers by lot number and RM IC certificate
+     * Returns heat numbers for a specific lot that matches the RM IC certificate
+     * @param lotNumber - Lot number
+     * @param rmCertificateNo - RM IC certificate number
+     * @return List of heat numbers
+     */
+    List<String> getHeatNumbersByLotNumber(String lotNumber, String rmCertificateNo);
+
+    /**
+     * Get Process IC certificate numbers for multiple RM IC certificates
+     * Returns all unique Process IC certificates that used any of the specified RM ICs
+     * @param rmCertificateNos - List of RM IC certificate numbers
+     * @return List of Process IC certificate numbers
+     */
+    List<String> getProcessIcCertificateNumbersByMultipleRmCertificates(List<String> rmCertificateNos);
+
+    /**
+     * Get Lot numbers by multiple RM IC and Process IC certificate numbers
+     * Returns all unique lot numbers that match any combination of the specified certificates
+     * @param rmCertificateNos - List of RM IC certificate numbers
+     * @param processCertificateNos - List of Process IC certificate numbers
+     * @return List of lot numbers
+     */
+    List<String> getLotNumbersByMultipleRmAndProcessCertificates(List<String> rmCertificateNos, List<String> processCertificateNos);
 }
 
