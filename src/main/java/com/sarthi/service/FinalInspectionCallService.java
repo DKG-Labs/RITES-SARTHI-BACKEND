e78d576723ef01ab07d3bedea84fefbbafd12efd
riyansh-dkg <riyansh@dkgrouplabs.com>
2026-02-08 17:00:19 +0530

    final po ser_no based

--- src/main/java/com/sarthi/service/FinalInspectionCallService.java
+++ src/main/java/com/sarthi/service/FinalInspectionCallService.java
@@ -16,20 +16,21 @@ public interface FinalInspectionCallService {
 
     /**
      * Create a new Final Inspection Call
-     * @param icRequest - Inspection Call basic details
-     * @param finalDetails - Final Inspection summary details
+     * 
+     * @param icRequest      - Inspection Call basic details
+     * @param finalDetails   - Final Inspection summary details
      * @param lotDetailsList - List of Final Inspection Lot Details
      * @return Created InspectionCall entity
      */
     InspectionCall createFinalInspectionCall(
             InspectionCallRequestDto icRequest,
             FinalInspectionDetailsRequestDto finalDetails,
-            List<FinalInspectionLotDetailsRequestDto> lotDetailsList
-    );
+            List<FinalInspectionLotDetailsRequestDto> lotDetailsList);
 
     /**
      * Get Process IC certificate numbers for Final Inspection Call dropdown
      * Filters by vendor and EP prefix
+     * 
      * @param vendorId - Vendor ID to filter
      * @return List of certificate numbers
      */
@@ -37,6 +38,7 @@ public interface FinalInspectionCallService {
 
     /**
      * Get RM IC numbers by Process IC certificate number
+     * 
      * @param certificateNo - Process IC certificate number
      * @return List of RM IC numbers
      */
@@ -44,25 +46,31 @@ public interface FinalInspectionCallService {
 
     /**
      * Get Lot numbers by RM IC number
+     * 
      * @param rmIcNumber - RM IC number
      * @return List of lot numbers
      */
     List<String> getLotNumbersByRmIcNumber(String rmIcNumber);
 
-    // ==================== NEW METHODS FOR REVERSED DROPDOWN FLOW ====================
+    // ==================== NEW METHODS FOR REVERSED DROPDOWN FLOW
+    // ====================
 
     /**
      * Get RM IC certificate numbers for Final Inspection Call dropdown
      * Returns CERTIFICATE_NO (e.g., "N/ER-01120005/RAJK") for display
-     * @param vendorId - Vendor ID to filter
+     * 
+     * @param poSerialNo - PO Serial Number to filter
      * @return List of RM IC certificate numbers
      */
-    List<String> getRmIcCertificateNumbers(String vendorId);
+    List<String> getRmIcCertificateNumbers(String poSerialNo);
 
     /**
      * Get Process IC certificate numbers by RM IC certificate
-     * Returns CERTIFICATE_NO (e.g., "N/EP-01170002/RAJK") for Process ICs that used the specified RM IC
-     * @param rmCertificateNo - RM IC certificate number (e.g., "N/ER-01120005/RAJK")
+     * Returns CERTIFICATE_NO (e.g., "N/EP-01170002/RAJK") for Process ICs that used
+     * the specified RM IC
+     * 
+     * @param rmCertificateNo - RM IC certificate number (e.g.,
+     *                        "N/ER-01120005/RAJK")
      * @return List of Process IC certificate numbers
      */
     List<String> getProcessIcCertificateNumbersByRmCertificate(String rmCertificateNo);
@@ -70,8 +78,11 @@ public interface FinalInspectionCallService {
     /**
      * Get Lot numbers by RM IC and Process IC certificate numbers
      * Converts certificates to IC numbers internally, then finds lots
-     * @param rmCertificateNo - RM IC certificate number (e.g., "N/ER-01120005/RAJK")
-     * @param processCertificateNo - Process IC certificate number (e.g., "N/EP-01170002/RAJK")
+     * 
+     * @param rmCertificateNo      - RM IC certificate number (e.g.,
+     *                             "N/ER-01120005/RAJK")
+     * @param processCertificateNo - Process IC certificate number (e.g.,
+     *                             "N/EP-01170002/RAJK")
      * @return List of lot numbers
      */
     List<String> getLotNumbersByRmAndProcessCertificates(String rmCertificateNo, String processCertificateNo);
@@ -79,7 +90,8 @@ public interface FinalInspectionCallService {
     /**
      * Get Heat numbers by lot number and RM IC certificate
      * Returns heat numbers for a specific lot that matches the RM IC certificate
-     * @param lotNumber - Lot number
+     * 
+     * @param lotNumber       - Lot number
      * @param rmCertificateNo - RM IC certificate number
      * @return List of heat numbers
      */
@@ -87,7 +99,9 @@ public interface FinalInspectionCallService {
 
     /**
      * Get Process IC certificate numbers for multiple RM IC certificates
-     * Returns all unique Process IC certificates that used any of the specified RM ICs
+     * Returns all unique Process IC certificates that used any of the specified RM
+     * ICs
+     * 
      * @param rmCertificateNos - List of RM IC certificate numbers
      * @return List of Process IC certificate numbers
      */
@@ -95,11 +109,13 @@ public interface FinalInspectionCallService {
 
     /**
      * Get Lot numbers by multiple RM IC and Process IC certificate numbers
-     * Returns all unique lot numbers that match any combination of the specified certificates
-     * @param rmCertificateNos - List of RM IC certificate numbers
+     * Returns all unique lot numbers that match any combination of the specified
+     * certificates
+     * 
+     * @param rmCertificateNos      - List of RM IC certificate numbers
      * @param processCertificateNos - List of Process IC certificate numbers
      * @return List of lot numbers
      */
-    List<String> getLotNumbersByMultipleRmAndProcessCertificates(List<String> rmCertificateNos, List<String> processCertificateNos);
+    List<String> getLotNumbersByMultipleRmAndProcessCertificates(List<String> rmCertificateNos,
+            List<String> processCertificateNos);
 }
-
