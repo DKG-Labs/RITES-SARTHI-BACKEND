3ccb64a20c9f891f885ab04534727aa42c24dac1
kudaykiran-99 <uday@dkgrouplabs.com>
2026-02-16 14:13:34 +0530

    deleted sleeper

--- src/main/java/com/sarthi/Sleeper/repository/SteamCubeSampleDeclarationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sarthi.Sleeper.repository;
-
-import com.sarthi.Sleeper.entity.SteamCubeSampleDeclaration;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SteamCubeSampleDeclarationRepository extends JpaRepository<SteamCubeSampleDeclaration, Long> {
-}
